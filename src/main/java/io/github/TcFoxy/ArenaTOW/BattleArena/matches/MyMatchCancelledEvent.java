ee4fc49743e5a194445b08a3ebf8854bbdab8836
TcFoxy <tomasscssantos@gmail.com>
2016-09-10 19:00:02 -0700

    Deleted the battle Arena stuff to do it with a seperate branch

--- src/main/java/io/github/TcFoxy/ArenaTOW/BattleArena/matches/MyMatchCancelledEvent.java
+++
@@ -1,9 +0,0 @@
-package io.github.TcFoxy.ArenaTOW.BattleArena.matches;
-
-import io.github.TcFoxy.ArenaTOW.BattleArena.competition.match.MyMatch;
-
-public class MyMatchCancelledEvent extends MyMatchEvent {
-	public MyMatchCancelledEvent(MyMatch match){
-		super(match);
-	}
-}
--- src/main/java/io/github/TcFoxy/ArenaTOW/BattleArena/matches/MyMatchCompletedEvent.java
+++
@@ -1,9 +0,0 @@
-package io.github.TcFoxy.ArenaTOW.BattleArena.matches;
-
-import io.github.TcFoxy.ArenaTOW.BattleArena.competition.match.MyMatch;
-
-public class MyMatchCompletedEvent extends MyMatchEvent {
-	public MyMatchCompletedEvent(MyMatch match){
-		super(match);
-	}
-}
